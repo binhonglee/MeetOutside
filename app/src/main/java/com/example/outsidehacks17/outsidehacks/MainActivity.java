9bdb33c06837d5071003e3fa05ff1c7cc68a312b
rykwan <Ray@raymonds-air.sfb.lyft-corp.net>
2017-07-29 13:39:50 -0700

    Initial commit

---
+++ app/src/main/java/com/example/outsidehacks17/outsidehacks/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.outsidehacks17.outsidehacks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
