1b276748a918c3d6763353ba21cd0c7246bbad81
tyler-donati <donati.tyler@gmail.com>
2017-07-30 12:48:25 -0400

    Undoing last commit.

--- app/src/main/java/com/example/outsidehacks17/outsidehacks/EditProfile.java
+++ app/src/main/java/com/example/outsidehacks17/outsidehacks/EditProfile.java
@@ -2,12 +2,21 @@ package com.example.outsidehacks17.outsidehacks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class EditProfile extends AppCompatActivity {
-
+    private UserFactory users;
+    private int currentUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+        users = this.getIntent().getParcelableExtra("UserFactory");
+        EditText etEmail = (EditText) findViewById(R.id.etEmail);
+        currentUser = this.getIntent().getIntExtra("id", 0);
+        etEmail.setText(users.get(currentUser).getEmail());
+
+
+        //EditText etPassword = (EditText) findViewById(R.id.etPassword);
     }
 }
