e94421fc359ce94b62f3ca6c111af7d04ce24eb4
rykwan <Ray@raymonds-air.sfb.lyft-corp.net>
2017-07-30 09:57:52 -0700

    autocomplete suggest first

--- app/src/main/java/com/example/outsidehacks17/outsidehacks/MainActivity.java
+++ app/src/main/java/com/example/outsidehacks17/outsidehacks/MainActivity.java
@@ -15,11 +15,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Intent loginScreen = new Intent(MainActivity.this, LoginScreen.class);
         loginScreen.putExtra("UserFactory", new UserFactory());
-       // Intent artistScreen = new Intent(MainActivity.this, ArtistsActivity.class);
-       // startActivity(artistScreen);
+        Intent artistScreen = new Intent(MainActivity.this, ArtistsActivity.class);
+        startActivity(artistScreen);
        // Intent eventsScreen = new Intent (MainActivity.this, ChooseEventsActivity.class);
        // startActivity(eventsScreen);
-        startActivity(loginScreen);
+       // startActivity(loginScreen);
         finish();
     }
 }
