939122fbc42159ff5e8b947cd9806675d90e2a20
tyler-donati <donati.tyler@gmail.com>
2017-07-30 00:52:43 -0400

    updated buttons in RegisterScreen

--- app/src/main/java/com/example/outsidehacks17/outsidehacks/RegisterScreen.java
+++ app/src/main/java/com/example/outsidehacks17/outsidehacks/RegisterScreen.java
@@ -1,7 +1,9 @@
 package com.example.outsidehacks17.outsidehacks;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -18,5 +20,17 @@ public class RegisterScreen extends AppCompatActivity {
         final EditText etPassword = (EditText) findViewById(R.id.etPassword);
 
         final Button bRegister = (Button) findViewById(R.id.bRegister);
+
+        bRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                UserFactory myUser = new UserFactory();
+                myUser.addUser(etName.getText().toString(), Integer.parseInt(etAge.getText().toString()), etEmail.getText().toString(), etPassword.getText().toString());
+                Intent registerIntent = new Intent(RegisterScreen.this, LoginScreen.class);
+                RegisterScreen.this.startActivity(registerIntent);
+            }
+        });
     }
 }
+
+
