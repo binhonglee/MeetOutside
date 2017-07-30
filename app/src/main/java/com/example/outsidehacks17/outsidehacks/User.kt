1062300022d3bdb2982048bc9bcbc1297d43469e
BinHong Lee <binhong@binhong.me>
2017-07-29 20:31:26 -0700

    Added ProfileDisplay

--- app/src/main/java/com/example/outsidehacks17/outsidehacks/User.kt
+++ app/src/main/java/com/example/outsidehacks17/outsidehacks/User.kt
@@ -1,12 +1,22 @@
 package com.example.outsidehacks17.outsidehacks
 
+import java.util.ArrayList
+
 class User {
     var name: String = ""
     var birthYear: Int = 0
     var email: String = ""
-    internal var password: String = ""
+    private var password: String = ""
+    var favArtists1: String = ""
+    var favArtists2: String = ""
+    var favArtists3: String = ""
+    var id: Int = 0
+    var image: String = ""
+    var toDo = ArrayList<String>()
 
-    constructor(name: String, birthYear: Int, email: String, password: String) {
+
+    constructor(id: Int, name: String, birthYear: Int, email: String, password: String) {
+        this.id = id
         this.name = name
         this.birthYear = birthYear
         this.email = email
--- app/src/main/java/com/example/outsidehacks17/outsidehacks/UserFactory.kt
+++ app/src/main/java/com/example/outsidehacks17/outsidehacks/UserFactory.kt
@@ -3,11 +3,15 @@ package com.example.outsidehacks17.outsidehacks
 import java.util.ArrayList
 
 class UserFactory {
-    internal var id: Int = 0
-    internal var users = ArrayList<User>()
+    private var id: Int = 0
+    private var users = ArrayList<User>()
 
     constructor() {
         id = 0
+    }
+
+    fun get(index: Int): User {
+        return users[search(index, 0, users.size)];
     }
 
     fun addUser(name: String, birthYear: Int, email: String, password: String): User {
@@ -16,8 +20,9 @@ class UserFactory {
                 .filter { it.email == email }
                 .forEach { throw NullPointerException() }
 
-        var newUser: User = User(name, birthYear, email, password)
+        var newUser: User = User(id, name, birthYear, email, password)
         users.add(newUser)
+        id++
         return newUser
     }
 
@@ -28,5 +33,46 @@ class UserFactory {
                 .forEach { return (it.checkPass(password)) }
 
         return false
+    }
+
+    fun update(oldUser: User, newUser: User): Boolean {
+        if (oldUser.id != newUser.id) {
+            return false
+        }
+
+        try {
+            users[search(oldUser.id, 0, id)] = newUser
+        } catch (e: Exception) {
+            throw NullPointerException()
+        }
+
+        return false
+    }
+
+    private fun search(index: Int, start: Int, end: Int): Int {
+        if (start == end && users[start].id == index)
+        {
+            return start
+        }
+
+        if (start >= end)
+        {
+            throw NullPointerException()
+        }
+
+        val currentId = (start + end) / 2
+
+        if (users[currentId].id == index)
+        {
+            return currentId
+        }
+        else if (users[currentId].id > index)
+        {
+            return search(index, start, currentId - 1)
+        }
+        else
+        {
+            return search(index, currentId + 1, end)
+        }
     }
 }
